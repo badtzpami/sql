71fa98734d2d51f1a9a49442925c5490bf074aeb
Pami <114650438+badtzpami@users.noreply.github.com>
2026-03-29 23:51:31 +0800

    Add files via upload

---
+++ Restaurant Orders/Obj1.sql
@@ -0,0 +1,41 @@
+USE restaurant_db;
+
+-- 1. View the mene_items tabLe.
+SELECT * FROM menu_items;
+
+-- 2. FInd the number of items on the menu.
+SELECT COUNT(*) FROM menu_items;
+
+-- 3. What are the least and most expensive items on the menu?
+SELECT * FROM menu_items
+ORDER BY price;
+
+SELECT * FROM menu_items
+ORDER BY price DESC;
+
+-- 4. How many Italian dishes are on the menu?
+SELECT COUNT(*) FROM menu_items
+WHERE category ="Italian";
+
+-- 5. What are the least abd most expensive Italian dishes on the menu?
+SELECT * FROM menu_items
+WHERE category = "Italian"
+ORDER BY price;
+
+SELECT * FROM menu_items
+WHERE category = "Italian"
+ORDER BY price DESC;
+
+-- 6. How many dishes are in each category?
+SELECT category,COUNT(menu_item_id) AS num_dishes
+FROM menu_items
+GROUP BY category;
+
+
+-- 7. What is the average dish price within each category?
+SELECT category,AVG(price) AS avg_price
+FROM menu_items
+GROUP BY category;
+
+
+
